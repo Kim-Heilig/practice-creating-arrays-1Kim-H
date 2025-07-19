ff62367bca0e22b004e4c6e099195611e91e2b40
Kim Heilig <kimmarieheilig@gmail.com>
2025-07-19 18:39:01 -0400

    completed exercise

---
+++ practice-creating-arrays.js
@@ -0,0 +1,22 @@
+/** @format */
+// ● An array of 5 grocery store items.
+// ● An array of 3 numbers representing prices.
+
+let groceryItems = ["apple", "banana", "corn", "eggs", "fish"];
+let pharmacyItems = ["hairbrush", "cough syrup", "chewing gum", "tissues"];
+let priceGroceryItems = [0.5, 2.0, 3.99];
+
+// 1. Log the entire array of grocery items.
+
+console.log("Grocery Store Items: ", groceryItems);
+// 2. Log the entire array of prices.
+console.log("Array of prices: ", priceGroceryItems);
+// 3. Log three individual items of your choice.
+console.log(
+  "Three Items in Grocery list: ",
+  groceryItems[1],
+  groceryItems[0],
+  groceryItems[3]
+);
+// 4. Log two prices of your choice.
+console.log("Two prices: ", priceGroceryItems[0], priceGroceryItems[2]);
